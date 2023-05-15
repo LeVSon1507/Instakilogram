685f10aa417c25f0550bd37ad56bc70602cd29a7
Le V Son <sonlvs1507@gmail.com>
2023-05-15 14:01:10 +0700

    create-base code mobile and config mongoDB server

---
+++ mobile/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+//internal navigation
+import HomePage from './src/scenes/homePage/HomePage';
+import LoginPage from './src/scenes/loginPage/LoginPage';
+
+const App = () => {
+   return (
+      <NavigationContainer>
+         <Stack.Navigator initialRouteName='Login'>
+            <Stack.Screen name='Home' component={HomePage} />
+            <Stack.Screen name='Login' component={LoginPage} />
+         </Stack.Navigator>
+      </NavigationContainer>
+   );
+};
+
+export default App;
