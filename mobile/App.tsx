67d860a2f7be3a334267c6623ac61da62fa3702d
7777 <vermas8900@gmail.com>
2023-05-21 12:25:58 +0700

    Test

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,8 +10,8 @@ import LoginPage from './src/scenes/loginPage/LoginPage';
 
 const App = () => {
    return (
-      <NavigationContainer>
-         <Stack.Navigator initialRouteName='Login'>
+      <NavigationContainer >
+         <Stack.Navigator initialRouteName='Login' screenOptions={{headerShown:false}}>
             <Stack.Screen name='Home' component={HomePage} />
             <Stack.Screen name='Login' component={LoginPage} />
          </Stack.Navigator>
