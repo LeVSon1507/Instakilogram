cb74253b5c9f18b848a6a2a6caf960981f553162
Quangtan <quantanc12345@gmail.com>
2023-06-02 10:13:42 +0700

    Fix login

--- server/index.js
+++ server/index.js
@@ -53,9 +53,9 @@ app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 6001;
+const PORT = process.env.PORT || 3001;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect("mongodb://localhost:27017/instagram", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
