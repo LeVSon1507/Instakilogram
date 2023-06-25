e188ef3ccb00633a6af9d8225e79090c16a2e482
7777 <vermas8900@gmail.com>
2023-06-25 22:57:53 +0700

    adding the model and auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,22 +1,21 @@
-const User = require("../models/userModel");
-const jwt = require("jsonwebtoken");
+import jwt from 'jsonwebtoken';
 
-const getUserFromToken = (accessToken, secretKey) => {
+export const verifyToken = async (req, res, next) => {
   try {
-    const decodedToken = jwt.verify(accessToken, secretKey);
-    const userId = decodedToken.userId;
-    return User.findById(userId);
+    let token = req.header('Authorization');
+
+    if (!token) {
+      return res.status(403).send('Access Denied');
+    }
+
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
   } catch (err) {
-    console.log(err);
-    return null;
+    res.status(500).json({ error: err.message });
   }
-};
-
-const generateAccessToken = (user) => {
-  return jwt.sign({ userId: user._id }, "secret_key", { expiresIn: "1h" });
-};
-
-module.exports = {
-  getUserFromToken,
-  generateAccessToken,
 };
