9a3f36526f81a27fe7c84bca7d4550f89eb93975
7777 <vermas8900@gmail.com>
2023-07-09 21:26:39 +0700

    Adding Component About.jsx

---
+++ client/src/components/profile/about/AboutMe.jsx
@@ -0,0 +1,59 @@
+import { Box, Button, Grid } from '@mui/material';
+import React, { useState } from 'react';
+import { Contact } from './component/Contact';
+import { Hobbies } from './component/Hobbies';
+import { Relationship } from './component/Relationship';
+import { Work } from './component/Work';
+import { Places } from './component/Places';
+
+export const AboutMe = () => {
+  const [activeComponent, setActiveComponent] = useState(null);
+
+  const data = [
+    { title: 'Contact and Basic Info', component: Contact },
+    { title: 'Hobbies and Interests', component: Hobbies },
+    { title: 'Family and Relationship', component: Relationship },
+    { title: 'Work and Education', component: Work },
+    { title: "Pleace You're Lived", component: Places },
+  ];
+
+  const handleClick = (component) => {
+    setActiveComponent(component);
+  };
+
+  const renderComponent = () => {
+    if (activeComponent) {
+      const Component = activeComponent.component;
+      return <Component />;
+    }
+
+    return <Contact />;
+  };
+
+  return (
+    <Grid container sx={{ gap: 2 }}>
+      <Grid item md={3} sx={{ background: 'white', padding: 3, borderRadius: 3, marginTop: 3, height: 270 }}>
+        <Box sx={{ width: '100%' }}>
+          {data.map((item) => (
+            <Button
+              key={item.title}
+              variant="text"
+              sx={{
+                fontSize: 19,
+                textAlign: 'start',
+                backgroundColor: activeComponent?.title === item.title ? 'rgba(46, 138, 216, 1)' : '',
+                color: activeComponent?.title === item.title ? 'white' : '',
+              }}
+              onClick={() => handleClick(item)}
+            >
+              {item.title}
+            </Button>
+          ))}
+        </Box>
+      </Grid>
+      <Grid item md={8} sx={{ background: 'white', padding: 3, borderRadius: 3, marginTop: 3 }}>
+        {renderComponent()}
+      </Grid>
+    </Grid>
+  );
+};
