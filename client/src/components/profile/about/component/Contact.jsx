40222c489052a6bddf7a58317d42f215d2c1a136
7777 <vermas8900@gmail.com>
2023-07-20 11:33:51 +0700

    adding the CRUD for the Contact

--- client/src/components/profile/about/component/Contact.jsx
+++ client/src/components/profile/about/component/Contact.jsx
@@ -1,5 +1,6 @@
-import { Grid } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { Grid, Button, TextField } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
 
 export const Contact = () => {
   const data = [
@@ -17,6 +18,24 @@ export const Contact = () => {
     { label: 'Joined:', value: 'April 31st, 2014' },
   ];
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedValue, setEditedValue] = useState('');
+  const [editIndex, setEditIndex] = useState(null);
+
+  const handleEditClick = (index) => {
+    setIsEditing(true);
+    setEditIndex(index);
+    setEditedValue(data[index].value);
+  };
+
+  const handleSaveClick = () => {
+    setIsEditing(false);
+  };
+
+  const handleCancelClick = () => {
+    setIsEditing(false);
+  };
+
   return (
     <Grid container>
       <Grid
@@ -30,11 +49,33 @@ export const Contact = () => {
         Personal Info
       </Grid>
       {data.map((item, index) => (
-        <Grid container gap={20} sx={{ fontSize: 20, marginTop: 3 }}>
+        <Grid container gap={20} sx={{ fontSize: 20, marginTop: 3, position: 'relative' }}>
+          {isEditing && index === editIndex ? (
             <React.Fragment key={index}>
               <Grid md={2}>{item.label}</Grid>
-              <Grid md={7}>{item.value}</Grid>
+              <Grid md={5}>
+                <TextField value={editedValue} onChange={(e) => setEditedValue(e.target.value)} />
+              </Grid>
+              <Button variant="text" onClick={handleSaveClick} sx={{ position: 'absolute', right: 0, top: 0 }}>
+                Save
+              </Button>
+              <Button variant="text" onClick={handleCancelClick} sx={{ position: 'absolute', right: 50, top: 0 }}>
+                Cancel
+              </Button>
             </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>
+              <Grid md={2}>{item.label}</Grid>
+              <Grid md={5}>{item.value}</Grid>
+              <Button
+                variant="text"
+                onClick={() => handleEditClick(index)}
+                sx={{ position: 'absolute', right: 0, top: -6 }}
+              >
+                <EditIcon />
+              </Button>
+            </React.Fragment>
+          )}
         </Grid>
       ))}
     </Grid>
